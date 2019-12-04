d0a4a753d1ab2ffc0a54be4e11b524e6ff893ed7
kent1121 <kent1121.sub@gmail.com>
2019-12-04 05:38:30 +0000

    layouts arranged

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,19 @@
 
 @section('content')
     @if (Auth::check())
-        {{ Auth::user()->name }}
+        <div class="row">
+            <div class="col-sm-2">
+                <h3>ユーザー</h3>
+                <p>{{ Auth::user()->name }}</p>
+            </div>
+            <div class="col-sm-10">
+                <h3>タスク一覧</h3>
+                {!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'btn btn-primary mb-3']) !!}
+                @if (count($tasks) > 0)
+                    @include('tasks.index', ['tasks' => $tasks])
+                @endif
+            </div>
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
